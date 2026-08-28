Amithtraj/cipher-core

[cr154] `ThumbnailTabHelper` moved into `TabFeatures`
`ThumbnailTabHelper::FromWebContents` and others were dropped and now it
is necessary to go through `ThumbnailTabHelper::From` to retrieve the
instance. `BraveTabFeatures` has been updated to unconditionally create
the instance.

Chromium changes:
https://chromium.googlesource.com/chromium/src/+/f7c68946aca7c2b7d975a63a12d9626e13292292

commit f7c68946aca7c2b7d975a63a12d9626e13292292
Author: Erik Chen <erikchen@chromium.org>
Date:   Tue Aug 18 09:45:53 2026 -0700

    Move ThumbnailTabHelper ownership into TabFeatures

    ThumbnailTabHelper attached itself to the WebContents via the
    WebContentsUserData pattern from TabHelpers::AttachTabHelpers(), gated
    on the kTabHoverCardImages feature. It is desktop-only with a single
    creation site, so it becomes a std::unique_ptr member of the tab's
    TabFeatures, created under the same feature gate and registered as
    UnownedUserData on the tab. Lookups go through
    ThumbnailTabHelper::From(tabs::TabInterface*); the production read
    sites already null-check and keep doing so (the helper still does not
    exist when the feature is disabled), and the ThumbnailTracker lookup
    now also tolerates non-tab WebContents.

    The discard flow is unchanged: the old helper stashes its thumbnail
    data on the incoming WebContents from AboutToBeDiscarded(), which the
    lifecycle code fires before the tab strip swaps contents, and
    TabFeatures::WillDiscardContents() then recreates the helper for the
    new contents, picking the stashed data up in its constructor.
    TabDataObserver::OnTabDiscarded() subscribes to the same discard
    callback list as TabFeatures but always after it (TabFeatures
    subscribes when the tab is created), so it keeps observing the
    recreated helper's thumbnail.

    Bug: 540709814
    Agent-Generated: true
    Change-Id: I08d3fc9bbfd579508db4110682dfd41374698f79
    Reviewed-on: https://chromium-review.googlesource.com/c/c

browser/ui/views/tabs/shared_pinned_tab_dummy_view_views.cc
@@ -15,6 +15,7 @@
 #include "chrome/browser/ui/browser_window/public/global_browser_collection.h"
 #include "chrome/browser/ui/thumbnails/thumbnail_tab_helper.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
+#include "components/tabs/public/tab_interface.h"
 #include "third_party/skia/include/core/SkPath.h"
 #include "third_party/skia/include/core/SkRect.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -60,8 +61,9 @@ SharedPinnedTabDummyViewViews::SharedPinnedTabDummyViewViews(
     content::WebContents* shared_contents,
     content::WebContents* dummy_contents)
     : SharedPinnedTabDummyView(shared_contents, dummy_contents),
-      thumbnail_(
-          ThumbnailTabHelper::FromWebContents(shared_contents)->thumbnail()),
+      thumbnail_(ThumbnailTabHelper::From(
+                     tabs::TabInterface::GetFromContents(shared_contents))
+                     ->thumbnail()),
       subscription_(thumbnail_->Subscribe()) {
   SetPaintToLayer();
   SetLayoutManager(std::make_unique<views::FlexLayout>())

mananpa4/brave-core

browser/ui/views/tabs/shared_pinned_tab_dummy_view_views.cc
@@ -15,6 +15,7 @@
 #include "chrome/browser/ui/browser_window/public/global_browser_collection.h"
 #include "chrome/browser/ui/thumbnails/thumbnail_tab_helper.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
+#include "components/tabs/public/tab_interface.h"
 #include "third_party/skia/include/core/SkPath.h"
 #include "third_party/skia/include/core/SkRect.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -60,8 +61,9 @@ SharedPinnedTabDummyViewViews::SharedPinnedTabDummyViewViews(
     content::WebContents* shared_contents,
     content::WebContents* dummy_contents)
     : SharedPinnedTabDummyView(shared_contents, dummy_contents),
-      thumbnail_(
-          ThumbnailTabHelper::FromWebContents(shared_contents)->thumbnail()),
+      thumbnail_(ThumbnailTabHelper::From(
+                     tabs::TabInterface::GetFromContents(dummy_contents))
+                     ->thumbnail()),
       subscription_(thumbnail_->Subscribe()) {
   SetPaintToLayer();
   SetLayoutManager(std::make_unique<views::FlexLayout>())